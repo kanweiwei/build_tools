283721d3979b69ccf6df316c5aa3b5073ca601ab
huangqingming <huangqingming@nutstore.net>
2022-04-21 20:39:44 +0800

    Change win desktop default compiling branch

--- tools/win/buils_win_desktop.bat
+++ tools/win/buils_win_desktop.bat
@@ -1,2 +1,2 @@
-python3 .\automate.py --update=0 --branch="master" --clean=0 --module="desktop" --qt-dir="C:\Qt\Qt5.8.0\5.8"
+python3 .\automate.py --update=0 --branch="nutstore" --clean=0 --module="desktop" --qt-dir="C:\Qt\Qt5.8.0\5.8"
 PAUSE
